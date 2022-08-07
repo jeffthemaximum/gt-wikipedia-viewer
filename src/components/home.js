85687fc4e865122db7388eac7bf2efdabdd5ad6c
Jeff Maxim <frey.maxim@gmail.com>
2022-08-07 10:34:22 -0400

    duck to do basic fetch of wikipedia api

--- src/components/home.js
+++ src/components/home.js
@@ -1,4 +1,8 @@
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import styled from 'styled-components'
+
+import wikipediaDucks from '../ducks/wikipedia'
 
 const StyledHeader = styled.div`
   background-color: ${props => props.theme.colors.grey0};
@@ -11,6 +15,28 @@ const Header = () => (
 )
 
 const Container = () => {
+  const dispatch = useDispatch()
+
+  const articles = useSelector(wikipediaDucks.selectors.articles)
+  const error = useSelector(wikipediaDucks.selectors.error)
+  const loading = useSelector(wikipediaDucks.selectors.loading)
+  const success = useSelector(wikipediaDucks.selectors.success)
+
+  useEffect(() => {
+    const shouldFetch = (
+      articles.length === 0 &&
+      !error &&
+      !loading &&
+      !success
+    )
+
+    if (shouldFetch) {
+      dispatch(wikipediaDucks.actions.getPageviews({ day: '31', month: '03', year: '2019' }))
+    }
+  }, [articles, dispatch, error, loading, success])
+
+  console.log({ articles })
+
   return (
     <Component />
   )
--- src/ducks/wikipedia/reducer.js
+++ src/ducks/wikipedia/reducer.js
@@ -6,20 +6,23 @@ export default function wikipedia (state = { articles: [] }, action) {
       return {
         ...state,
         error: false,
-        loading: true
+        loading: true,
+        success: false
       }
     case actionTypes.GET_PAGEVIEWS_ERROR:
       return {
         ...state,
         error: true,
-        loading: false
+        loading: false,
+        success: false
       }
     case actionTypes.GET_PAGEVIEWS_SUCCESS:
       return {
         ...state,
         articles: action.articles,
         error: false,
-        loading: false
+        loading: false,
+        success: true
       }
     default:
       return state
